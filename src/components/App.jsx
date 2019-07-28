d15d7b6cbc3348fc5131b06df047f43f91142803
AshVivier <ash.set.vivier@gmail.com>
2019-07-28 19:33:19 +1200

    Play with getting geo to Google Maps

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 
 import React, { Component } from 'react';
 import Contacts from './Contacts';
-import { HashRouter as Router, Route } from 'react-router-dom'
-import Map from './Map'
+// import { HashRouter as Router, Route } from 'react-router-dom'
+
+
 
 class App extends Component {
 
@@ -19,25 +20,22 @@ class App extends Component {
       .catch(console.log)
   }
 
-
   render() {
     return (
 
 
-
-      <Router>
-
         <Contacts contacts={this.state.contacts} />
 
-        <Route exact path='/' component={this.state.Contacts} />
-
-        <Route path='/Map' component={Map} />
-
-      </Router>
-
-
+     
     )
   }
 }
+
+
+// var button = document.querySelector("button");
+
+// button.addEventListener("click", () => {
+//   window.parent.open('https://maps.google.com/?q=latitude-value,longitude-value')
+//   });
 
 export default App
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,29 +1,45 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Map from './Map'
+// import { Link } from 'react-router-dom'
+
+ // var button = document.querySelector("button");
+
+  // button.addEventListener("click", () => {
+  //   window.open('https://maps.google.com/?q=latitude-value,longitude-value')
+  //   });
+
+
 
 const Contacts = ({ contacts }) => {
 
-  return (
+  const url = new URL('https://example.com?');
+  const params = new URLSearchParams(url.search)
+  params.append("{contact.address.geo.lat},{contact.address.geo.lat}");
+
+  return (  
+
     <div>
       <center><h1>Contact List</h1></center>
       {contacts.map((contact) => (
         <center>  <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Name: {contact.name}</h5>
+          <div className="card-body">
+            <h5 className="card-title">Name: {contact.name}</h5>
 
-            <h6 class="card-subtitle mb-2">Email: {contact.email}</h6>
+            <h6 className="card-subtitle mb-2">Email: {contact.email}</h6>
 
-            <Link to='./Map'><button >Address: {contact.address.street}<br />
-              {contact.address.suite} <br /> {contact.address.city}</button></Link><br />
+            <button>Address: {contact.address.street}<br />
+              {contact.address.suite} <br /> {contact.address.city}</button>
+              
+              {/* <input type='button' onClick={window.open('https://maps.google.com/?q=')} value='click here to visit home page' ></input> */}
 
-            <p class="card-text">Website: {contact.website}</p>
+            <p className="card-text">Website: {contact.website}</p>
 
           </div>
+          {/* {contact.address.geo.lat} */}
         </div></center>
       ))}
-      <Map />
+
     </div>
+  
   )
 };
 
